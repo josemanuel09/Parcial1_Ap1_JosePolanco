389fb7866e3b4dd9be149969ebf62150f8db56d0
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-12 18:17:52 -0400

    Creando Modelo de metas

---
+++ Model/Metas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_JosePolanco.Model
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio ")]
+        public DateTime Fecha{ get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio ")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio ")]
+        public decimal Monto { get; set; }
+    }
+}
