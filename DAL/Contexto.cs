d9f57f3b33f6c2ca557cb8fec632fb05b35527cb
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-12 18:32:28 -0400

    Creando Contexto de Metas.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_JosePolanco.Model;
+
+namespace Parcial1_Ap1_JosePolanco.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Metas> Metas { get; set; }
+
+    }
+}
